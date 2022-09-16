79a3b0fc18ca48f434f3874ce5ae18f70f2dd9ad
covaciuandrei <81052535+covaciuandrei@users.noreply.github.com>
2022-09-16 14:19:37 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+// console.log(buttons);
+
+buttons.map(button => {
+    button.addEventListener('click', e => {
+        // console.log('clicked');
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = '';
+                break;
+            case '←':
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0,-1);
+                }
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);//ia textul din display si exectua ca javascript
+                }   catch {
+                    display.innerText = 'Error!';
+                }
+                 
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
